c2fe13b54e664b6beb45937b9668b0741ed30974
Bestine <bestineokinda@gmail.com>
2022-06-21 15:15:19 +0300

    Using beautifulSoup to find the info-able to find the main heading

--- findcity.py
+++ findcity.py
@@ -1,1 +1,10 @@
-print("hello Mustafa")
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+	url="https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_and_their_capitals_in_native_languages",
+)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+title = soup.find(id="firstHeading")
+print(title.string)
